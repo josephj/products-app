9af748d1ad97f0036f8ac5ec6206803bb1994898
josephj <josephj6802@gmail.com>
2019-01-16 00:02:09 +1100

    Clean up

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,11 @@
 import { routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createRootReducer from './reducer';
 
-export const history = createBrowserHistory();
+export const history = createHashHistory();
 
 let middlewares = [routerMiddleware(history), thunk];
 const env = process.env.REACT_APP_ENV || process.env.NODE_ENV;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Redirect, Route, Switch } from 'react-router'; // react-router v4
 import { ConnectedRouter } from 'connected-react-router';
 import configureStore, { history } from './configureStore';
-
+import App from './App';
 import './index.css';
-import Products from './modules/products';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <Switch>
-          <Route path="/products" render={() => <Products />} />
-          <Redirect to="/products" />} />
-        </Switch>
-      </div>
+      <App />
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
