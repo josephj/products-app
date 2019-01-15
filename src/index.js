eba8de1d66a91cd36fd93f64b09fcfa3751f51e2
josephj <josephj6802@gmail.com>
2019-01-15 21:23:32 +1100

    Install react-router and redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router'; // react-router v4
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from './configureStore';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <App />} />
+          <Route render={() => <div>Miss</div>} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
