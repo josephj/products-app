fe17faa623950108f0b0dad4d6dcdd2ac11e3992
josephj <josephj6802@gmail.com>
2019-01-15 23:46:30 +1100

    Updates

---
+++ precache-manifest.c3a833be76472817708d8a314bf2f08c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabc0d1ee5d2133b1901",
+    "url": "/products-app/static/js/runtime~main.eabc0d1e.js"
+  },
+  {
+    "revision": "6989759f0c1c64392585",
+    "url": "/products-app/static/js/main.6989759f.chunk.js"
+  },
+  {
+    "revision": "7f7fa3f55c94a50ef9b4",
+    "url": "/products-app/static/js/1.7f7fa3f5.chunk.js"
+  },
+  {
+    "revision": "6989759f0c1c64392585",
+    "url": "/products-app/static/css/main.5118c8a3.chunk.css"
+  },
+  {
+    "revision": "db3b3837fc09929dcfa337fecccda34b",
+    "url": "/products-app/index.html"
+  }
+];
