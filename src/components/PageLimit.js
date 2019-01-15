a1875bc8f53960008883ba1b62145367ee8542d8
josephj <josephj6802@gmail.com>
2019-01-16 08:04:28 +1100

    Enhance the style (WIP)

--- src/components/PageLimit.js
+++ src/components/PageLimit.js
@@ -1,8 +1,8 @@
 import React from 'react';
-export default ({ onChange, value }) => {
+export default ({ onChange, value, ...otherProps }) => {
   const sizes = [4, 8, 12, 24, 48, 96, 198];
   return (
-    <select value={value} onChange={onChange}>
+    <select value={value} onChange={onChange} {...otherProps}>
       {sizes.map((size, i) => (
         <option key={i} value={size}>
           {size} per page
--- src/modules/products/ProductList.js
+++ src/modules/products/ProductList.js
@@ -37,42 +37,72 @@ export class ProductList extends Component {
       onLimitChange,
       onPageChange
     } = this.props;
+
     return (
-      <div>
-        <h1>All Products</h1>
-        <PageLimit value={limit} onChange={onLimitChange} />
-        <Pager
-          total={total}
-          limit={limit}
-          value={page}
-          onChange={onPageChange}
-        />
+      <Wrapper>
+        <Header>
+          <div>
+            <Title>All Products</Title>
+            <Info>24 Products</Info>
+          </div>
+          <div style={{ marginLeft: 'auto' }}>
+            <PageLimit value={limit} onChange={onLimitChange} />
+          </div>
+        </Header>
         <List>
           {data.map(product => (
             <Item key={product.id}>
-              <Img src={product.product_image} alt="Product" />
-              <br />
-              {product.product_name}
-              <br />
-              {product.description}
-              <br />${product.price}
+              <ItemWrapper>
+                <ItemMedia>
+                  <Img src={product.product_image} alt="Product" />
+                </ItemMedia>
+                <ItemInfo>
+                  <b style={{ color: '#666' }}>{product.product_name}</b>
+                  <br />
+                  {product.description}
+                  <br />
+                  <b>{product.price}</b>
+                </ItemInfo>
+              </ItemWrapper>
             </Item>
           ))}
         </List>
-        <PageLimit value={limit} onChange={onLimitChange} />
-        <Pager
-          total={total}
-          limit={limit}
-          value={page}
-          onChange={onPageChange}
-        />
-      </div>
+        <Footer>
+          <Pager
+            total={total}
+            limit={limit}
+            value={page}
+            onChange={onPageChange}
+          />
+        </Footer>
+      </Wrapper>
     );
   }
 }
 
+const Wrapper = styled.div``;
+
+const Header = styled.div`
+  align-items: flex-end;
+  border-bottom: solid 1px #eaeaea;
+  display: flex;
+  padding: 5px 6px;
+  margin: 0 0 15px;
+`;
+const Title = styled.h1`
+  color: #666;
+  font-family: 'Raleway', sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0 0 5px;
+`;
+const Info = styled.div`
+  color: #999;
+  font-size: 13px;
+`;
 const List = styled.ul`
-  margin: 0;
+  margin: 0 -10px 0 0;
+  padding: 0;
   list-style-type: none;
   &:after {
     content: '';
@@ -81,19 +111,40 @@ const List = styled.ul`
   }
 `;
 const Item = styled.li`
-  float: left;
-  width: 300px;
-  height: 300px;
-  background: #ffc;
+  display: inline-block;
+  width: 25%;
+`;
+const ItemWrapper = styled.div`
+  background: #fff;
+  border: solid 1px #eaeaea;
   margin: 0 10px 10px 0;
+  height: 280px;
+`;
+const ItemMedia = styled.div`
+  padding: 12px 15px 8px;
   text-align: center;
 `;
+const ItemInfo = styled.div`
+  border-top: solid 1px #f2f2f2;
+  font-size: 13px;
+  line-height: 1.4;
+  padding: 10px 15px;
+  text-align: left;
+  // TODO
+  white-space: nowrap;
+  overflow: hidden;
+  width: 90%;
+  text-overflow: ellipsis;
+`;
 const Img = styled.img`
-  background: #000;
   display: inline-block;
-  width: 200px;
-  height: 200px;
+  width: 180px;
+  height: 180px;
   object-fit: contain;
 `;
-
+const Footer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 20px 0;
+`;
 export default connectStore(ProductList);
