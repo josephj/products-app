4470cfcf8f154d079b24bceffdf4bd3ce30551a5
josephj <josephj@users.noreply.github.com>
2019-01-22 22:32:25 +1100

    #2 Component - Pagination (#20)

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,47 +1,111 @@
 import React from 'react';
 import styled from 'styled-components';
-import Pagination from 'react-js-pagination';
+import PropTypes from 'prop-types';
 
-export default ({ total, limit, page, onChange }) => {
-  return (
-    <Wrapper>
-      <Pagination
-        activePage={page}
-        hideFirstLastPages
-        itemsCountPerPage={limit}
-        totalItemsCount={total}
-        pageRangeDisplayed={3}
-        prevPageText="< Previous page"
-        nextPageText="Next page >"
-        onChange={onChange}
-      />
-    </Wrapper>
-  );
-};
+export function getVisiblePages(page, totalPages, range) {
+  const pages = [...Array(totalPages)].map((val, i) => i + 1);
+  const isFirstPage = page === 1 || page - Math.floor(range / 2) <= 1;
+  const isLastPage = page === totalPages || page + range > totalPages;
 
-const Wrapper = styled.span`
+  if (isFirstPage) {
+    return pages.slice(0, range);
+  } else if (isLastPage) {
+    return pages.slice(-range);
+  } else {
+    const i = pages.indexOf(page);
+    return pages.slice(i - Math.floor(range / 2), i + Math.ceil(range / 2));
+  }
+}
+
+export default class Pager extends React.Component {
+  static propTypes = {
+    limit: PropTypes.number,
+    page: PropTypes.number,
+    range: PropTypes.number,
+    total: PropTypes.number,
+    onChange: PropTypes.func
+  };
+  static defaultProps = {
+    limit: 8,
+    page: 1,
+    range: 3,
+    total: 0,
+    onChange: () => {}
+  };
+  handlePageClick = nextPage => e => {
+    const { limit, onChange, page, total } = this.props;
+    const totalPages = Math.ceil(total / limit);
+
+    e.preventDefault();
+
+    if (nextPage === page || nextPage < 1 || nextPage > totalPages) {
+      return;
+    }
+
+    onChange(nextPage);
+  };
+  render() {
+    const { total, limit, page, range, ...otherProps } = this.props;
+
+    if (!total) {
+      return null;
+    }
+
+    const totalPages = Math.ceil(total / limit);
+    const visiblePages = getVisiblePages(page, totalPages, range);
+    return (
+      <Wrapper {...otherProps}>
+        <Item disabled={page === 1}>
+          <Link href="#" onClick={this.handlePageClick(page - 1)}>
+            &lt; Previous page
+          </Link>
+        </Item>
+        {visiblePages.map(i => (
+          <Item key={i} active={i === page}>
+            <Link href="#" onClick={this.handlePageClick(i)}>
+              {i}
+            </Link>
+          </Item>
+        ))}
+        <Item disabled={page === totalPages}>
+          <Link href="#" onClick={this.handlePageClick(page + 1)}>
+            Next page &gt;
+          </Link>
+        </Item>
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.ul`
   display: inline-block;
-  .pagination {
-    font-size: 13px;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    a {
-      color: #666;
-      font-weight: 500;
-      text-decoration: none;
-    }
-    li {
-      border-bottom: 3px solid transparent;
-      display: inline-block;
-      padding: 5px 10px;
-      &.disabled a {
+  font-size: 13px;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+const Link = styled.a`
+  color: #666;
+  display: inline-block;
+  font-weight: 500;
+  padding: 5px 10px;
+  text-decoration: none;
+`;
+const Item = styled.li`
+  border-bottom: 3px solid transparent;
+  display: inline-block;
+  ${props =>
+    props.active &&
+    `
+      background: #fff;
+      border-color: #333;
+    `};
+  ${Link} {
+    ${props =>
+      props.disabled &&
+      `
+        cursor: default;
         color: #ccc;
-      }
-      &.active {
-        background: #fff;
-        border-bottom: 3px solid #333;
-      }
-    }
+      `};
   }
 `;
